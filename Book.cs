bb999b2db60932c39d7936c96b51da746236554c
Muavia542 <68521427+Muavia542@users.noreply.github.com>
2020-09-14 09:07:52 +0500

    Add files via upload

---
+++ Book.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+using System.Data.Entity;
+
+
+namespace ClassWorkCRUD.Models
+{
+    public class Book
+    {
+        public int ID { get; set; }
+        public string Title { get; set; }
+        public DateTime ReleaseDate { get; set; }
+        public string Theme { get; set; }
+        public decimal Price { get; set; }
+    }
+    public class BookDBContext : DbContext
+    {
+        public DbSet<Book> Books { get; set; }
+    }
+
+
+}
